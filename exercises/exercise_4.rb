6fd19d803e6bf5c009924de304ef3ea0beeb020c
Ivana Lee <lee.ivana@hotmail.com>
2018-12-04 17:33:51 +0000

    Finished exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -12,7 +12,12 @@ Store.create!(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, wom
 Store.create!(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true);
 
 @mens_store = Store.where(mens_apparel: true)
+@womens_store = Store.where("annual_revenue < ? AND womens_apparel = ?", 1000000, true)
 
 @mens_store.each do |store|
-  puts store.name
+  puts "Sells mens clothes : #{store.name}"
+end
+
+@womens_store.each do |store|
+  puts "Sells womens clothing : #{store.name}"
 end
